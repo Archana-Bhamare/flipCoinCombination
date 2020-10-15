aff43617eea6431de19b7b2b965121efbe78d8e9
Archana-Bhamare <archusonar2@gmail.com>
2020-10-15 16:40:15 +0530

    Added : Singlet Combination_Loop

--- flipCoin.sh
+++ flipCoin.sh
@@ -1,9 +1,24 @@
 #!/bin/bash -x
-
-toss=$((RANDOM%2))
-if [ $toss -eq 0 ]
-then
-	echo "Head wins"
-else
-	echo "Tail Wins"
-fi
+#"---Singlet Combination---"
+head=0
+count=0
+declare -A coin
+for ((i=0;i<5;i++))
+do
+	toss=$((RANDOM%2))
+	if [ $toss -eq 0 ]
+	then	
+		echo "Head"
+		((head++))
+	else
+		echo "Tail"
+		((tail++))	
+	fi
+done
+coin[head]=$head
+coin[tail]=$tail
+echo "${!coin[@]} : ${coin[@]}"
+percentage=`echo $head | awk '{print ($1/5)*100}'`
+echo "Percentage of Head : $percentage%"
+percentage2=`echo $tail | awk '{print ($1/5)*100}'`
+echo "Percentage of Tail : $percentage2%"
